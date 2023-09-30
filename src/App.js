e40cedb9a5387f306d65491129af5875a3d07eea
Amos Chen <achen002@mt.feitian.edu>
2023-09-30 12:52:36 +0000

    Include image

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import moonFestivalImage from './images/moon-festival.webp';
+
 export function App() {
-    return <h1 className="text-blue-400">Hello world Again!</h1>;
+    return (
+        <>
+            <img src={moonFestivalImage} className='w-72' />
+            <h1 className="text-amber-600">Hello World Again!</h1>
+        </>
+    );
 }
